77e96d9bac0aeac27121b65d4572260026456766
P. S. Clarke <fishy.dynasty@pscl4rke.net>
2025-12-22 20:28:53 +0000

    Placeholder output

--- webapp.py
+++ webapp.py
@@ -1,14 +1,20 @@
 
 
-from quart import Quart
+from quart import Quart, render_template
 
 
-APP = Quart("webapp")
+APP = Quart(
+    "webapp",
+    template_folder="jinja",
+)
 
 
 @APP.route("/")
 async def index():
-    return "Hello World"
+    return await render_template(
+        "output.html",
+        title="Output 99",
+    )
 
 
 if __name__ == "__main__":
